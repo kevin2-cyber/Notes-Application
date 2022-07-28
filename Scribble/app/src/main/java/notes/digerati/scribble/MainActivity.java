9e14281879f670671d04f50eaa50ed4a3728e333
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-28 16:13:01 +0000

    initial commit

---
+++ Scribble/app/src/main/java/notes/digerati/scribble/MainActivity.java
@@ -0,0 +1,15 @@
+package notes.digerati.scribble;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
