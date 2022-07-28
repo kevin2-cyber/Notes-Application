aa82df7e6d2d73feff57abf5ab541c5b17f9710a
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-07-28 17:11:08 +0000

    tried some stuffs

--- Scribble/app/src/main/java/notes/digerati/scribble/MainActivity.java
+++ Scribble/app/src/main/java/notes/digerati/scribble/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(R.string.app_name);
     }
 }
