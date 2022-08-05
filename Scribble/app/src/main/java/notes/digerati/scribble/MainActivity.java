bb9a1bf062a147a850c6ab9baec854cc7290e0bf
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-05 16:43:57 +0000

    new

--- Scribble/app/src/main/java/notes/digerati/scribble/MainActivity.java
+++ Scribble/app/src/main/java/notes/digerati/scribble/MainActivity.java
@@ -31,7 +31,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.textView.setText(R.string.app_name);
-        Button loginBtn = findViewById(R.id.login_btn);
-        Button signBtn = findViewById(R.id.sign_up_btn);
     }
 }
