ebfa12d078a7a37296480792bcadc440317e44de
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-12 12:23:14 +0000

    checking if user is logged in already or not

--- Scribble/app/src/main/java/notes/digerati/scribble/view/AddScreenActivity.java
+++ Scribble/app/src/main/java/notes/digerati/scribble/view/AddScreenActivity.java
@@ -5,11 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import notes.digerati.scribble.databinding.ActivityAddScreenBinding;
 
 public class AddScreenActivity extends AppCompatActivity {
 
     ActivityAddScreenBinding binding;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +26,34 @@ public class AddScreenActivity extends AppCompatActivity {
             Intent intent = new Intent(this, WorkSpaceActivity.class);
             startActivity(intent);
         });
+
+        // init auth
+        auth = FirebaseAuth.getInstance();
+        checkUser();
+
+        // handle click, logout
+        binding.btnSignOut.setOnClickListener( view -> {
+            auth.signOut();
+            checkUser();
+        });
+    }
+
+    private void checkUser() {
+
+        // check if user is logged in or not
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+
+            // user is not null, user is logged in, get user info
+            String email = user.getEmail();
+
+            // set to text view
+            binding.tvEmail.setText(email);
+        } else {
+
+            //user is null, user not logged in go to login activity
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
     }
 }
