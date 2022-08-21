12fa48ad4e9ba6d3b8dfc92c031ca2f8446487a9
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-21 22:42:48 +0000

    trying to work this around hope it works

--- Scribble/app/src/main/java/notes/digerati/scribble/adapter/RecyclerViewAdapter.java
+++ Scribble/app/src/main/java/notes/digerati/scribble/adapter/RecyclerViewAdapter.java
@@ -1,16 +1,30 @@
 package notes.digerati.scribble.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import notes.digerati.scribble.R;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder>  {
+
+    private Context context;
+    private int[] mPlaceList;
+
+    public RecyclerViewAdapter(Context context, int[] mPlaceList) {
+        this.context = context;
+        this.mPlaceList = mPlaceList;
+    }
+
     @NonNull
     @Override
     public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.save_area, parent, false);
+        return new RecyclerViewHolder(view);
     }
 
     @Override
